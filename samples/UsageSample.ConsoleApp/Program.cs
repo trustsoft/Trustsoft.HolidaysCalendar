2b13c0a92203fb32cbd7627f14db99498fc4c385
m.sukhanov <sukhanov.maxim@gmail.com>
2024-11-11 23:41:14 +0300

    Update usage sample.

--- samples/UsageSample.ConsoleApp/Program.cs
+++ samples/UsageSample.ConsoleApp/Program.cs
@@ -6,21 +6,69 @@ using Trustsoft.HolidaysCalendar.DataProviders;
 
 internal static class Program
 {
-    private static readonly int CurrentYear = DateTime.Now.Year;
     private static readonly Random Rnd = new Random(Environment.TickCount);
 
     static void Main()
     {
-        Console.WriteLine("HolidaysCalendar Sample Usage");
+        Console.WriteLine("//-------------------------------//");
+        Console.WriteLine("// HolidaysCalendar Sample Usage //");
+        Console.WriteLine("//-------------------------------//");
 
         IHolidaysCalendar calendar = new HolidaysCalendar();
         var dataProvider = new XmlCalendarDataProvider();
         calendar.Initialize(dataProvider);
+
+        Console.WriteLine();
+        Console.WriteLine("HolidaysCalendar.IsHoliday(DateOnly date) usage");
+        Console.WriteLine();
         var number = 0;
-        foreach (var date in GetTestDays())
+
+        var dates = GetTestDays().ToList();
+
+        foreach (var date in dates)
         {
             var result = calendar.IsHoliday(date);
             Console.WriteLine($"#{++number:00}: {date:dd.MM.yyyy} is holiday = {result}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("HolidaysCalendar.IsWeekend(DateOnly date) usage");
+        Console.WriteLine();
+        number = 0;
+        foreach (var date in dates)
+        {
+            var result = calendar.IsWeekend(date);
+            Console.WriteLine($"#{++number:00}: {date:dd.MM.yyyy} is weekend = {result}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("HolidaysCalendar.IsWorkingWeekend(DateOnly date) usage");
+        Console.WriteLine();
+        number = 0;
+        foreach (var date in dates)
+        {
+            var result = calendar.IsWorkingWeekend(date);
+            Console.WriteLine($"#{++number:00}: {date:dd.MM.yyyy} is working weekend = {result}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("HolidaysCalendar.GetNextWorkingDay(DateOnly date) usage");
+        Console.WriteLine();
+        number = 0;
+        foreach (var date in dates)
+        {
+            var result = calendar.GetNextWorkingDay(date);
+            Console.WriteLine($"#{++number:00}: {date:dd.MM.yyyy} next working day is = {result}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("HolidaysCalendar.AdjustForHolidays(DateOnly date) usage");
+        Console.WriteLine();
+        number = 0;
+        foreach (var date in dates)
+        {
+            var result = calendar.AdjustForHolidays(date);
+            Console.WriteLine($"#{++number:00}: {date:dd.MM.yyyy} adjusted for holidays day is = {result}");
         }
 
         Console.ReadLine();
@@ -30,11 +78,12 @@ internal static class Program
     {
         var list = new List<DateOnly>();
 
-        for (int i = 0; i < 21; i++)
+        for (int i = 0; i < 3; i++)
         {
             list.Add(GetRandomDateOnly());
         }
-        list.Add(new DateOnly(CurrentYear, 1, 2));
+        list.Add(new DateOnly(2024, 1, 2));
+        list.Add(new DateOnly(2024, 5, 8));
 
         foreach (var dateOnly in list.OrderBy(d => d))
         {
@@ -45,7 +94,7 @@ internal static class Program
     private static DateOnly GetRandomDateOnly()
     {
         var month = Rnd.Next(1, 12);
-        var day = Rnd.Next(1, DateTime.DaysInMonth(CurrentYear, month));
-        return new DateOnly(CurrentYear, month, day);
+        var day = Program.Rnd.Next(1, DateTime.DaysInMonth(2024, month));
+        return new DateOnly(2024, month, day);
     }
 }
