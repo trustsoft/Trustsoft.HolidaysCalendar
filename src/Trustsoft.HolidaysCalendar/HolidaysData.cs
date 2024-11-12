007918ec8bc2ad90289539f11788e7b280466b78
m.sukhanov <sukhanov.maxim@gmail.com>
2024-11-12 18:52:05 +0300

    Fix HolidayData.IsEmpty implementation.

--- src/Trustsoft.HolidaysCalendar/HolidaysData.cs
+++ src/Trustsoft.HolidaysCalendar/HolidaysData.cs
@@ -5,16 +5,16 @@
 //  <date>09.11.2024</date>
 // -------------------------Copyright © 2024 M.Sukhanov. All rights reserved.-------------------------
 
-using Trustsoft.HolidaysCalendar.Contracts;
-
 namespace Trustsoft.HolidaysCalendar;
+
+using Trustsoft.HolidaysCalendar.Contracts;
 
 internal class HolidaysData(IReadOnlyList<DateOnly> holidays, IReadOnlyList<DateOnly> workingWeekends)
         : IHolidaysData
 {
-    public IReadOnlyList<DateOnly> Holidays { get; } = holidays;
+    public IReadOnlyList<DateOnly> Holidays { get; } = holidays ?? [];
 
-    public IReadOnlyList<DateOnly> WorkingWeekends { get; } = workingWeekends;
+    public IReadOnlyList<DateOnly> WorkingWeekends { get; } = workingWeekends ?? [];
 
-    public bool IsEmpty => !(this.Holidays?.Count > 0 && this.WorkingWeekends?.Count > 0);
+    public bool IsEmpty => !(this.Holidays.Count > 0 || this.WorkingWeekends.Count > 0);
 }
