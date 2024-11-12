15b581f55510e3d2e5b4619aabc720bf3734e65e
m.sukhanov <sukhanov.maxim@gmail.com>
2024-11-12 17:09:48 +0300

    Add Test Project.

---
+++ tests/Trustsoft.HolidaysCalendar.Tests/Trustsoft.HolidaysCalendar.Tests/HolidayDataTests.cs
@@ -0,0 +1,66 @@
+﻿// -------------------------Copyright © 2024 M.Sukhanov. All rights reserved.-------------------------
+//  <copyright file="HolidayDataTests.cs" author="M.Sukhanov">
+//      Copyright © 2024 M.Sukhanov. All rights reserved.
+//  </copyright>
+//  <date>12.11.2024</date>
+// -------------------------Copyright © 2024 M.Sukhanov. All rights reserved.-------------------------
+
+namespace Trustsoft.HolidaysCalendar.Tests;
+
+using Trustsoft.HolidaysCalendar.Contracts;
+
+[TestClass]
+public class HolidayDataTests
+{
+    [TestMethod]
+    public void HolidayDataIsNullTest1()
+    {
+        IHolidaysData data = new HolidaysData([], null);
+
+        Assert.IsTrue(data.IsEmpty);
+
+        data = new HolidaysData(null, []);
+
+        Assert.IsTrue(data.IsEmpty);
+
+        data = new HolidaysData(null, null);
+
+        Assert.IsTrue(data.IsEmpty);
+    }
+
+    [TestMethod]
+    public void HolidayDataIsEmptyTest1()
+    {
+        var list1 = new List<DateOnly>();
+        var list2 = new List<DateOnly>();
+
+        IHolidaysData data = new HolidaysData(list1, list2);
+        Assert.IsTrue(data.IsEmpty);
+    }
+
+    [TestMethod]
+    public void HolidayDataIsNotEmptyTest1()
+    {
+        var list1 = new List<DateOnly>
+        {
+            new DateOnly(2024, 10, 10)
+        };
+
+        IHolidaysData data = new HolidaysData(list1, new List<DateOnly>());
+
+        Assert.IsFalse(data.IsEmpty);
+    }
+
+    [TestMethod]
+    public void HolidayDataIsNotEmptyTest2()
+    {
+        var list2 = new List<DateOnly>
+        {
+            new DateOnly(2024, 10, 10)
+        };
+
+        IHolidaysData data = new HolidaysData(new List<DateOnly>(), list2);
+
+        Assert.IsFalse(data.IsEmpty);
+    }
+}
---
+++ tests/Trustsoft.HolidaysCalendar.Tests/Trustsoft.HolidaysCalendar.Tests/HolidaysCalendarTests.cs
@@ -0,0 +1,108 @@
+// -------------------------Copyright © 2024 M.Sukhanov. All rights reserved.-------------------------
+//  <copyright file="HolidaysCalendarTests.cs" author="M.Sukhanov">
+//      Copyright © 2024 M.Sukhanov. All rights reserved.
+//  </copyright>
+//  <date>12.11.2024</date>
+// -------------------------Copyright © 2024 M.Sukhanov. All rights reserved.-------------------------
+
+namespace Trustsoft.HolidaysCalendar.Tests;
+
+using Trustsoft.HolidaysCalendar.Contracts;
+using Trustsoft.HolidaysCalendar.DataProviders;
+
+[TestClass]
+public class HolidaysCalendarTests
+{
+    private IHolidaysCalendar calendar = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        var holidaysCalendar = new HolidaysCalendar();
+        var dataProvider = new XmlCalendarDataProvider();
+        holidaysCalendar.Initialize(dataProvider);
+        this.calendar = holidaysCalendar;
+    }
+
+    [TestMethod]
+    public void CreateAndInitializeTest()
+    {
+        IHolidaysCalendar holidaysCalendar = new HolidaysCalendar();
+        holidaysCalendar.Initialize(new XmlCalendarDataProvider());
+    }
+
+    [DataTestMethod]
+    [DataRow("2024.01.01")]
+    [DataRow("2024.01.02")]
+    [DataRow("2024.01.03")]
+    [DataRow("2024.01.04")]
+    [DataRow("2024.01.05")]
+    [DataRow("2024.01.06")]
+    [DataRow("2024.01.07")]
+    [DataRow("2024.01.08")]
+    [DataRow("2024.02.23")]
+    [DataRow("2024.03.08")]
+    [DataRow("2024.05.01")]
+    [DataRow("2024.05.09")]
+    [DataRow("2024.06.12")]
+    [DataRow("2024.11.04")]
+    public void IsHolidayTest(string dateOnlyString)
+    {
+        var date = DateOnly.ParseExact(dateOnlyString, "yyyy.MM.dd");
+        Assert.IsTrue(this.calendar.IsHoliday(date));
+    }
+
+    [DataTestMethod]
+    [DataRow("2024.01.09")]
+    [DataRow("2024.01.10")]
+    [DataRow("2024.02.22")]
+    [DataRow("2024.03.07")]
+    [DataRow("2024.04.27")]
+    [DataRow("2024.05.02")]
+    public void IsNotHolidayTest(string dateOnlyString)
+    {
+        var date = DateOnly.ParseExact(dateOnlyString, "yyyy.MM.dd");
+        Assert.IsFalse(this.calendar.IsHoliday(date));
+    }
+
+    [DataTestMethod]
+    [DataRow("2024.01.01", "2024.01.09")]
+    [DataRow("2024.01.02", "2024.01.09")]
+    [DataRow("2024.01.03", "2024.01.09")]
+    [DataRow("2024.01.04", "2024.01.09")]
+    [DataRow("2024.01.05", "2024.01.09")]
+    [DataRow("2024.01.06", "2024.01.09")]
+    [DataRow("2024.01.07", "2024.01.09")]
+    [DataRow("2024.01.08", "2024.01.09")]
+    [DataRow("2024.02.23", "2024.02.26")]
+    [DataRow("2024.02.25", "2024.02.26")]
+    [DataRow("2024.04.29", "2024.05.02")]
+    [DataRow("2024.06.12", "2024.06.13")]
+    [DataRow("2024.11.04", "2024.11.05")]
+    public void AdjustForHolidaysTest(string dateOnlyString, string expectedString)
+    {
+        var date = DateOnly.ParseExact(dateOnlyString, "yyyy.MM.dd");
+        var expected = DateOnly.ParseExact(expectedString, "yyyy.MM.dd");
+
+        var actual = this.calendar.AdjustForHolidays(date);
+        Assert.AreEqual(actual, expected);
+    }
+
+    [DataTestMethod]
+    [DataRow("2024.01.13")]
+    public void IsWeekendTest(string dateOnlyString)
+    {
+        var date = DateOnly.ParseExact(dateOnlyString, "yyyy.MM.dd");
+        Assert.IsTrue(this.calendar.IsWeekend(date));
+    }
+
+    [DataTestMethod]
+    [DataRow("2024.02.23")]
+    [DataRow("2024.04.27")]
+    [DataRow("2024.12.28")]
+    public void IsNotWeekendTest(string dateOnlyString)
+    {
+        var date = DateOnly.ParseExact(dateOnlyString, "yyyy.MM.dd");
+        Assert.IsFalse(this.calendar.IsWeekend(date));
+    }
+}
