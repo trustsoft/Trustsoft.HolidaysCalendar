6333c2ea6403fb9c1f890de0f64d67a33b42ba29
m.sukhanov <sukhanov.maxim@gmail.com>
2024-11-15 20:42:02 +0300

    Add IFallbackDataProvider contract.

---
+++ src/Trustsoft.HolidaysCalendar/Contracts/IFallbackDataProvider.cs
@@ -0,0 +1,12 @@
+﻿// -------------------------Copyright © 2024 M.Sukhanov. All rights reserved.-------------------------
+//  <copyright file="IFallbackDataProvider.cs" author="M.Sukhanov">
+//      Copyright © 2024 M.Sukhanov. All rights reserved.
+//  </copyright>
+//  <date>15.11.2024</date>
+// -------------------------Copyright © 2024 M.Sukhanov. All rights reserved.-------------------------
+
+namespace Trustsoft.HolidaysCalendar.Contracts;
+
+public interface IFallbackDataProvider : IHolidaysDataProvider
+{
+}
