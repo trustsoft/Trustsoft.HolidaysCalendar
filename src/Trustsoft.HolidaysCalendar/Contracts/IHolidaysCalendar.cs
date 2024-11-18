b3598e426c34da26780d87342115539e59f7ea1b
m.sukhanov <sukhanov.maxim@gmail.com>
2024-11-18 08:42:02 +0300

    Fix paramref tags in XML Doc comments.

--- src/Trustsoft.HolidaysCalendar/Contracts/IHolidaysCalendar.cs
+++ src/Trustsoft.HolidaysCalendar/Contracts/IHolidaysCalendar.cs
@@ -10,35 +10,36 @@ namespace Trustsoft.HolidaysCalendar.Contracts;
 public interface IHolidaysCalendar
 {
     /// <summary>
-    ///   Adjusts the specified <param name="date" /> to ensure it is not a holiday or a weekend.
+    ///   Adjusts the specified <paramref name="date" /> to ensure it is not a holiday or a weekend.
     /// </summary>
     /// <param name="date"> The date to adjust for. </param>
     /// <returns> The date adjusted for holidays and weekends. </returns>
     DateOnly AdjustForHolidaysAndWeekends(DateOnly date);
 
     /// <summary>
-    ///   Gets the next working day after specified <param name="date" />.
+    ///   Gets the next working day after specified <paramref name="date" />.
     /// </summary>
     /// <param name="date"> The date to check for. </param>
     /// <returns> The next working day. </returns>
     DateOnly GetNextWorkingDay(DateOnly date);
 
     /// <summary>
-    ///   Determines whether the specified <param name="date" /> is holiday.
+    ///   Determines whether the specified <paramref name="date" /> is holiday.
     /// </summary>
     /// <param name="date"> The date to check for. </param>
     /// <returns> <c> true </c> if the specified date is holiday; otherwise, <c> false </c>. </returns>
     bool IsHoliday(DateOnly date);
 
     /// <summary>
-    ///   Determines whether the specified date is a weekend, taking into account working weekends.
+    ///   Determines whether the specified <paramref name="date" /> is a weekend, taking into account working
+    ///   weekends.
     /// </summary>
     /// <param name="date"> The date to check for. </param>
     /// <returns> <c> true </c> if the specified date is a weekend; otherwise, <c> false </c>. </returns>
     bool IsWeekend(DateOnly date);
 
     /// <summary>
-    ///   Determines whether the specified <param name="date" /> is working weekend.
+    ///   Determines whether the specified <paramref name="date" /> is working weekend.
     /// </summary>
     /// <param name="date"> The date to check for. </param>
     /// <returns> <c> true </c> if the specified date is working weekend; otherwise, <c> false </c>. </returns>
